2e717f27157673a8ddc2545d036c3a44584359fa
Llaseras <160228907+Llaseras@users.noreply.github.com>
2024-06-19 03:53:42 +0200

    Update Tag.ino

--- TR/Tag/Tag/Tag.ino
+++ TR/Tag/Tag/Tag.ino
@@ -1,4 +1,4 @@
-//Part especifica que es fa depenent de si el dispositiu actúa com un Tag o com un ancla.
+  //Part especifica que es fa depenent de si el dispositiu actúa com un Tag o com un ancla.
 // Aqui es defineix que el dispositiu és un Tag. (Si volguessim definir l'ancla, llavors ficariem #define IS_ANCHOR).
 #define IS_TAG
 
@@ -26,44 +26,23 @@
 // Aquí es fa servir el MAC, un tipus d'adreça eletronica comunment utilitzat.
 #define DEVICE_ADDRESS "02:00:00:00:00:00:77:76"
 
-
-// Aquesta part només afecta si esta fent servir una pantlla LCD en aquell dispositiu en especific.
-//En la següent linia LiquidCrystal és el nom que li fiquem a la pantalla LCD.
-//I els numeros dins dels parentesis fan referencia als pins assignats al dispositiu.
-//Els pins també fan referencia al següent ordre de pins del LCD: RS, E, D4, D5, D6, D7.
-LiquidCrystal lcd(13, 12, 5, 18, 19, 21);
-
-#ifdef IS_TAG
-
-  // Ara el que anem a fer és crear una variable que tingui la adreça MAC del nostre ESP32.
-  // S'ha de ficar la adreça MAC del receptor. Cada placa ESP32 té una direcció MAC dferent. 64:B7:08:CA:C0:7C
-  uint8_t receiverAddress[] = {0x64, 0xB7, 0x08, 0xCA, 0xC0, 0x7C};
-
-  // Aquí creem un objecte anomenat uwb_data que emmagatzemarà informació amb la estrucutra de MyLink, que ve del document "link.h".
-  struct MyLink *uwb_data;
-
-  // Aquí es crea una variable del tipus long (32 bits) enlloc del tipus int(16 bits). El unsigned fa que només sigui positiu.
-  // Després a la variable se li asigan el nom lastUpdateTime i per ultim se loi asigna el valor 0.
-  unsigned long lastUpdateTime = 0;
-
-  // Aquí es crea una variable de tipus int amb nomes valors positius.
-  // A la variable se li asigna el nom udateInterval i se li asigna el valor 200.
-  unsigned int updateInterval = 200;
-
-#endif
-
 // Aquí es defineix una variable de tipus char(cadena de text) que té 6 caracters.
 // A aquesta variable se li asigna el nom shortAddress que després es fara servir com una Adreça abreviada. (Per generar menys carrega al processador).
 char shortAddress[6];
+
+struct MyLink *uwb_data;
 
 // setup és la funció que inicia el codi i que serveix per a posar en marxa el dispositiu.
 //En el setup es fiquen totes les funcions ue en algun moemtn es cridaran desde una llibreria.
 void setup() {
 
+  // Això el que fa és determinar la freqüencia a la que la placa i l'ordinador es comunicaran
+  Serial.begin(115200);
+
   // En aquesta linia es crida la funció begin desde la llibreria SPI.
   // I se li asignan els pins: (18 per al rellotge, 19 per sortida de informació, 5 per entrada de informació).
   // En aquest cas l'argument són els pins que es faran servir.
-  SPI.begin(18, 19, 5);
+  SPI.begin(18, 19, 23);
   
   // En aquesta linia següent sinicialitza la comunicació, desde la llibreria DWRanging es crida la funció initcomunication().
   //En aquesta funció, el 27 és el pin Reset
@@ -86,86 +65,14 @@ void setup() {
   // Aquesta funció s'activara quan un dispositiu deixi de ser detectat.
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
 
-  // En la següent linia el que es fa és de la llibreria lcd(LiquidCrystal) es crida la funció begin.
-  // Es donen els arguments 16 i 2 que són el numero de columans i files que tindra la pantalla.
-  lcd.begin(16,2);
+  // Aquesta funcio el que fa iniciar la comunicació DW1000 amb el mode de TAG.
+  // La comunicació DW-1000 es una especifica per poder localitzar mitjançant la trilateració.
+  // La funció té tres arguments. Device_Adress que hem definit abans, el mode de transmissió de dades i el filtre de rang.
+  // El filtre de rang no és necessari així que l'hem selecionat com a 0.
+  DW1000Ranging.startAsTag(DEVICE_ADDRESS, DW1000.MODE_LONGDATA_RANGE_ACCURACY, false);
 
-  lcd.print("Estoy encendido");
-  delay(3000);
-
-  // En la següent linia de codi el que es fa és cridar la funció clear desde la llibreria lcd.
-  // En aquesta funció no fa falta introduir cap argument. Ja que no hi ha cap valor etern necessari en la funció.
-  lcd.clear();
-
-
-  // Aqui el que es fa és executar el codi només en cas de que l'objecte estigui definit com a Tag.
-  #ifdef IS_TAG
-
-    // Aquesta funcio el que fa iniciar la comunicació DW1000 amb el mode de TAG.
-    // La comunicació DW-1000 es una especifica per poder localitzar mitjançant la trilateració.
-    // La funció té tres arguments. Device_Adress que hem definit abans, el mode de transmissió de dades i el filtre de rang.
-    // El filtre de rang no és necessari així que l'hem selecionat com a 0.
-    DW1000Ranging.startAsTag(DEVICE_ADDRESS, DW1000.MODE_LONGDATA_RANGE_ACCURACY, false);
-
-    // En la següent linia el que fem és generar text en la pantalla. El que estigui entre les dues "" serà el que es mostrara a la pantalla LCD.
-    lcd.print("Modo: TAG");
-
-
-    //Deixem temps per a llegir el missatge abans d'eliminar-lo de la pantalla.
-    delay(3000);
-    lcd.clear();
-
-    // Aquí se li asigna a la variable uwb_data el valor que retorna la funció init_link que esta dins del document link.h.
-    uwb_data = init_link();
-
-
-    // Aquí el que es fa és iniciar una conexió Wifi amb el dispositiu amb el que vols compartir la teva ubicació.
-    // A la següent linia de codi el que passa és que es defineix la comunicació Wifi com si el dispositiu fos un un client regular de la xarxa.
-    // El que significa és que es connecta com si fos un movil o un ordinador. L'unic que fa falta saber i especificar és el nom de la xarxa i la contrasenya.
-    WiFi.mode(WIFI_STA);
-    WiFi.begin("La red de Peta", "contraseña");
-
-    // La següent linia el que fa és imprimir conectant mentres que el dispositiu es conecta a la red WIFI.
-    // Aquest bucle funciona creant el tipic patro de tres punts que van apareixent mentres diu conectando.
-    while (WiFi.status() != WL_CONNECTED) {
-      lcd.print("Conectando");
-      delay(250);
-      lcd.print(".");
-      delay(250);
-      lcd.print(".");
-      delay(250);
-      lcd.print(".");
-      delay(250);
-      lcd.clear();
-    }
-
-
-    // Aquí s'inicialitza ESP_NOW.
-    if (esp_now_init() != ESP_OK) {
-      lcd.clear();
-      lcd.print("Ha Fallao");
-      delay(3000);
-      lcd.clear();
-      return;
-    }
-
-
-    // En la següent linia el que es fa és crear una nova variable amb nome: peerInfo, que té la estructura esp_npm_peer_info_t.
-    esp_now_peer_info_t peerInfo;
- 
-    // La següent linia el que fa és copiar desde la adreça MAC del receptor els numeros darrere d'un 0x i desp´res posar-los a la variable peerInfo.
-    // El tercer argument que és 6, vol dir que hi ha sis numeros darrere d'un 0x.
-    memcpy(peerInfo.peer_addr, receiverAddress, 6);
-
-    // Aquestes dues linies de codi següents el que fan és determinal el canal en el que el es comunicaran els dispositius i que la conexio no estarà encriptada.
-    peerInfo.channel = 0;
-    peerInfo.encrypt = false;
-
-    // Abans de conectar els dos disposiitius s'ha d'agregar el receptor com a un dispositiu conegut.
-    esp_now_add_peer(&peerInfo);
-
-
-  #endif
+  // Aquí se li asigna a la variable uwb_data el valor que retorna la funció init_link que esta dins del document link.h.
+  uwb_data = init_link();
 
   // I ara el que fenm és calcular una adreça més curta per a poder fer servir una adreça més accessible.
 
@@ -178,26 +85,6 @@ void setup() {
   // el següent argument és "%02X%02X" les dues comiilles indiquen que és una cadena de text. Mentress que l'interior es pot dividir en dues parts iguals.
   // Cada una de les parts és %02X que vol dir que ha de ser un string amb dos digits. (En cas de que només sigui un digit es fica un 0 a la esquerra)
   sprintf(shortAddress, "%02X%02X", currentShortAddress[1], currentShortAddress[0]);
-
-  lcd.clear();
-
-  // Ara a la pantalla LCD imprimim la adreça curta del dipositiu.
-  lcd.print("Short Address:");
-  lcd.setCursor(0, 1);
-  lcd.print(shortAddress);
-
-  // Deixem un temps per a que l'ususari pugui llegir el missatge
-  delay(3000);
-
-  // Ara tornarem a deixar la pantalla lcd com estava.
-  lcd.clear();
-
-  // Per ultim, el que fem és avisar de que ja s'ha acabat la posada en marcha i ara comença el codi principal.
-  lcd.print("Finiquitao");
-  delay(3000);
-  lcd.clear();
-
-  pinMode(2, OUTPUT);
 }
 
 
@@ -209,13 +96,16 @@ void loop() {
   // La funció és capaç de saber si el dispositiu és una ancla o un Tag i depenent del que sigui fa una cosa o una altra.
   DW1000Ranging.loop();
 
-
+  // La funció send_json el que fa és enviar el docment json a través del port serial.
   send_json(uwb_data);
+
+  // Ara per últim anem a afegir un petit punt que el que fará és millorar l'estalbi energetic i alliberará carga del TAG.
+  // Això ho podem fer augmentant el temps entre itineració i itineració del codi.
+  delay(100)
 }
 
 
 // En la següent funció el que es fa és enviar l'arxiu Json.
-#ifdef IS_TAG
 void send_json(struct MyLink *p) {
 
   // En la següent linia el que es fa és crear un document Json amb una capacitat de 500 bytes.
@@ -257,40 +147,17 @@ void send_json(struct MyLink *p) {
   // Ara es crea una cadena de caracters anomenada Jsonbuffer que será qui guardara el document Json serialitzat.
   char jsonBuffer[500];
 
+
   // La següent linia de codi el que fa és transformar el document Json en una cadena de text Json, el qual és més facil de trasmetre.
   // En aquest cas els arguments són doc i udp, que són el arxiu a serialitzar i el destinatari on s'enviara aquest "arxiu".
   // També es crea una variable amb que té com a valor el tamany de JsonBuffer.
   size_t len = serializeJson(doc, jsonBuffer);
 
-  // Esp_now té la caracteristica de que només pot enviar paquest de fins 250 bytes. Per això mateix, em de dividir el nostre Json en diferents paquets
-  // En la següent linia es pot veure com calculem el numero de paquets que ha de tindre el nostre Json.
-  int numPackets = len / 250;
-  if (len % 250 != 0) {
-    numPackets++;
-  }
 
-  // Ara el que hem de fer és enviar els paquets per separat. El for és una altra manera de fer loops.
-  // En la següent linia el que es pot llegir és que es crea la variable i. i Per cada vegada que es fa una volta a i se li suma 1.
-  // Per últim es veu com el loop es repeteix mentres que i segui més petita al numero de paquets.
-  for (int i = 0; i < numPackets; i++) {
-
-    // Ara hem de calcular en qquin byte ha de començar el paquet i en quin ha d'acabar.
-    int Start = i * 250;
-    // En aquesta linia següent el que es fa és mirar quin és minim de dos valors, si la llargada del nsotre Json o la suma del començament i el final.
-    // D'aquesta manera no es llegeix més enllà del final del Json Buffer.
-    int Finish = min(Start + 250, (int)len);
-
-    // I per ultim s'envia el paquet que pertoca. Aquesta part d'aqui (uint8_t *)&, serveix per a que la funció revi les dades amb el format que pertoca.
-    // Auqesta part d'aqui Final - Start s'asegura de que només senvii les dades que nosaltres volem enviar. També señala el tamany en bytes del se senviará.
-    esp_now_send(receiverAddress, (uint8_t *)&jsonBuffer[Start], Finish - Start);
-  }
-  digitalWrite(2, HIGH);
-  delay(100);
-  digitalWrite(2, LOW);
-
+  Serial.println(jsonBuffer);
+  Serial.print(DW1000Ranging.getDistantDevice()->getRange());
 
 } // Aquí s'acava la funció
-#endif
 
 
 // Aquí es definirá la funció newRange() que es fa servir a la part d'adalt del codi.
@@ -298,22 +165,13 @@ void newRange() {
   // Aquí el que es fa és crear una cadena de caracters anomenada buffer. A aquesta cadena se li fiquen com a numero maxim de caracters 21.
   char buffer[21];
 
-  // Aquí per si de cas vuidem la pantalla lcd.
-  lcd.clear();
-
   // La següent linia de codi serveix per "omplir" la cadena de caracters buffer amb la adreça curta del dispositiu.
   // El problema és que la funció DW1000Ranging.getDistantDevice()->getShortAddress() retorna un valor en format int.
   // Així que hem de transformar aquest valor int en una cadena de caracters.
   snprintf(buffer, sizeof buffer, "%04x", DW1000Ranging.getDistantDevice()->getShortAddress());
 
-  // La següent linia de codi el que fa és imprimir en la pantalla el numero del dispositiu al que s'esta connectant.
-  lcd.print(buffer);
-
   // En aquesta linia el que esta fent el nostre dispositiu és grabar dins de la cadena buffer la distancia que hi ha entre el dispositiu i la ancla en qüestió.
   int ret = snprintf(buffer, sizeof buffer, "%.2f", DW1000Ranging.getDistantDevice()->getRange());
-
-  // I per "últim el que fa aquesta linia és "
-  lcd.print(buffer);
 
   // Aquesta linia de text només s'activa en cas de que el dispositiu sigui un Tag.
   // La aegüent linia de codi el que fa és actualitzar la informació de la comunicació entre el dispositiu i l'ancla.
